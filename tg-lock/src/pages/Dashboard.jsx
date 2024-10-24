6d524220fec5b00859020a8c0273295a422cea39
saadaoui-salah <salahsaadaoui8@gmail.com>
2024-10-24 17:36:22 -0400

    add: Qr cpde feat

--- tg-lock/src/pages/Dashboard.jsx
+++ tg-lock/src/pages/Dashboard.jsx
@@ -1,10 +1,82 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { TOTP } from "totp-generator";
 import Logo from "../logo.jpg";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useApp } from "../api/app";
 import { useLogout } from "../api/auth";
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+const qrcodeRegionId = "html5qr";
+
+// Creates the configuration object for Html5QrcodeScanner.
+const createConfig = (props) => {
+  let config = {};
+  if (props.fps) {
+    config.fps = props.fps;
+  }
+  if (props.qrbox) {
+    config.qrbox = props.qrbox;
+  }
+  if (props.aspectRatio) {
+    config.aspectRatio = props.aspectRatio;
+  }
+  if (props.disableFlip !== undefined) {
+    config.disableFlip = props.disableFlip;
+  }
+  console.log(config);
+  return config;
+};
+
+const Html5QrcodePlugin = (props) => {
+  useEffect(() => {
+    // when component mounts
+    const config = createConfig(props);
+    const verbose = props.verbose === true;
+    // Suceess callback is required.
+    if (!props.qrCodeSuccessCallback) {
+      throw "qrCodeSuccessCallback is required callback.";
+    }
+    const html5QrcodeScanner = new Html5QrcodeScanner(
+      qrcodeRegionId,
+      config,
+      verbose
+    );
+    html5QrcodeScanner.render(
+      props.qrCodeSuccessCallback,
+      props.qrCodeErrorCallback
+    );
+
+    // cleanup function when component will unmount
+    return () => {
+      html5QrcodeScanner.clear().catch((error) => {
+        console.error("Failed to clear html5QrcodeScanner. ", error);
+      });
+    };
+  }, []);
+
+  return (
+    <div
+      className="w-[300px] h-[300px] flex items-center justify-center flex-col"
+      id={qrcodeRegionId}
+    />
+  );
+};
+
+const PopUP = ({ children, close }) => {
+  return (
+    <div className="absolute flex items-center justify-center bg-black bg-opacity-50 z-50 w-full h-full">
+      <div className="bg-white relative items-center gap-5 flex flex-col rounded-md px-4 py-4">
+        <span
+          onClick={() => close()}
+          className="border cursor-pointer z-[100] top-[12px] text-white bg-red-500 text-center absolute right-[12px] rounded-full py-[1px] px-2 font-bold"
+        >
+          X
+        </span>
+        {children}
+      </div>
+    </div>
+  );
+};
 
 const Dashboard = () => {
   const { error, submit, apps, deleteApp } = useApp();
@@ -28,54 +100,66 @@ const Dashboard = () => {
     generateTOTPs();
     const interval = setInterval(() => {
       generateTOTPs();
-      console.log(timeRemaining);
     }, 1000);
 
     return () => clearInterval(interval); // Clean up the interval on component unmount
   }, [apps]);
   const [open, setOpen] = useState(false);
+  const [scan, setScan] = useState(false);
   const onSubmit = () => {
     setOpen(false);
     submit(data);
   };
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log(decodedText);
+  };
+
   return (
     <>
+      {scan && (
+        <PopUP close={() => setScan(false)}>
+          <Html5QrcodePlugin
+            fps={10}
+            qrbox={250}
+            disableFlip={false}
+            qrCodeSuccessCallback={onNewScanResult}
+          />
+        </PopUP>
+      )}
       {open && (
-        <div className="absolute flex items-center justify-center bg-black bg-opacity-50 z-50 w-full h-full">
-          <div className="bg-white items-center gap-5 flex flex-col rounded-md px-4 py-4">
-            <p className="text-black mb-4 font-bold text-xl">Create App</p>
-            <input
-              required
-              type="text"
-              onChange={(e) =>
-                setData({
-                  ...data,
-                  name: e.target.value,
-                })
-              }
-              placeholder="Name"
-              className="px-2 py-1 rounded-md text-md border text-black bg-gray-600 bg-opacity-30 "
-            />
-            <input
-              type="text"
-              required
-              onChange={(e) =>
-                setData({
-                  ...data,
-                  secret_key: e.target.value,
-                })
-              }
-              placeholder="Secret key"
-              className="px-2 py-1 rounded-md text-md border text-black bg-gray-600 bg-opacity-30 "
-            />
-            <button
-              onClick={() => onSubmit()}
-              className="text-white bg-black w-24 py-1 rounded-md shadow-md hover:shadow-sm"
-            >
-              Save
-            </button>
-          </div>
-        </div>
+        <PopUP close={() => setOpen(false)}>
+          <p className="text-black mb-4 font-bold text-xl">Create App</p>
+          <input
+            required
+            type="text"
+            onChange={(e) =>
+              setData({
+                ...data,
+                name: e.target.value,
+              })
+            }
+            placeholder="Name"
+            className="px-2 py-1 rounded-md text-md border text-black bg-gray-600 bg-opacity-30 "
+          />
+          <input
+            type="text"
+            required
+            onChange={(e) =>
+              setData({
+                ...data,
+                secret_key: e.target.value,
+              })
+            }
+            placeholder="Secret key"
+            className="px-2 py-1 rounded-md text-md border text-black bg-gray-600 bg-opacity-30 "
+          />
+          <button
+            onClick={() => onSubmit()}
+            className="text-white bg-black w-24 py-1 rounded-md shadow-md hover:shadow-sm"
+          >
+            Save
+          </button>
+        </PopUP>
       )}
       <div className="w-full relative h-full bg-white">
         <div className="flex items-center justify-between px-4">
@@ -94,6 +178,12 @@ const Dashboard = () => {
             className={`text-white bg-red-500 disabled:bg-red-400 disabled:shadow-none w-24 py-1 rounded-md shadow-md hover:shadow-sm`}
           >
             Delete
+          </button>
+          <button
+            onClick={() => setScan(true)}
+            className="text-white bg-black w-32 py-1 rounded-md shadow-md hover:shadow-sm"
+          >
+            Scan QR Code
           </button>
           <button
             onClick={() => setOpen(true)}
@@ -116,7 +206,7 @@ const Dashboard = () => {
               }
               className={`border cursor-pointer ${
                 selected.includes(totp.id)
-                  ? "hover:bg-white bg-gray-200"
+                  ? " bg-gray-200"
                   : "hover:bg-gray-200 bg-white"
               } items-center mt-12 text-black w-full flex justify-between shadow-md px-4 py-2 rounded-md`}
             >
